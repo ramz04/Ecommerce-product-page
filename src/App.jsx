45889e358ec34209b3e5dd9825d2a33222a4867f
Abdul-Rahman Wunpini <wunpini95@gmail.com>
2023-02-28 17:56:34 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-3xl font-bold">
+      This is the App
+    </div>
+  )
+}
+
+export default App
