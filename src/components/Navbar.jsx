3c7554817d7e8c54afc062370ca3d5178f647b93
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-03-30 05:21:48 +0000

    Added Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,30 @@ function Navbar() {
     const [open, setOpen] = useState(false)
 
   return (
-    <div>
-        <div className='nav--menu px-6 py-4 flex items-center shadow-md justify-between'>
-            <div className='flex gap-4 items-center'>
+    <div className='md:px-[8rem]'>
+        <div className='nav--menu px-6 py-4 md:py-0 h-[8rem] md:border-b-2 flex items-center  justify-between'>
+            <div className='flex gap-4 items-center h-full'>
                 <button className='md:hidden' onClick={() => setOpen(!open)}>
                     <img src={icon_menu}  alt='icon-menu'/>
                 </button>
-                <img src={logo} alt='logo' />
-            </div>
-            <div className='flex gap-4 items-center'>
-                <div>
-                    <img src={cart} />
+                <div className='flex gap-10 items-center h-full'>
+                    <a href="/"><img src={logo} alt='logo' /></a>
+                    <div className='hidden md:flex font-kumbh text-Darkgrayishblue h-full text-sm'>
+                        <ul className='flex gap-8 items-center'>
+                            <li className='h-full flex items-center hover:border-b-4 hover:border-Orange hover:text-Verydarkblue transition-transform ease-in-out '><a href="#">Collections</a></li>
+                            <li className='h-full flex items-center hover:border-b-4 hover:border-Orange hover:text-Verydarkblue transition-transform ease-in-out '><a href="#">Men</a></li>
+                            <li className='h-full flex items-center hover:border-b-4 hover:border-Orange hover:text-Verydarkblue transition-transform ease-in-out '><a href="#">Women</a></li>
+                            <li className='h-full flex items-center hover:border-b-4 hover:border-Orange hover:text-Verydarkblue transition-transform ease-in-out '><a href="#">About</a></li>
+                            <li className='h-full flex items-center hover:border-b-4 hover:border-Orange hover:text-Verydarkblue transition-transform ease-in-out '><a href="#">Contacts</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <img src={avatar} className='w-[30px]' />
+            </div>
+            <div className='flex gap-4 md:gap-10 items-center'>
+                <div>
+                    <img src={cart} classname='md:w-[40px]' />
+                </div>
+                <img src={avatar} className='w-[30px] md:w-[40px] hover:ring-2 rounded-full hover:ring-Orange' />
             </div>
         </div>
         <div aria-checked={!open} className='nav--drawer h-screen aria-checked:-translate-x-full transition-transform ease-in-out fixed bg-orange w-[70%] z-40 left-0 top-0'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        base: '16px'
+      },
+      colors: {
+        Orange: "hsl(26, 100%, 55%)",
+        Paleorange: "hsl(25, 100%, 94%)",
+        Verydarkblue: "hsl(220, 13%, 13%)",
+        Darkgrayishblue: "hsl(219, 9%, 45%)",
+        Grayishblue: "hsl(220, 14%, 75%)",
+        Lightgrayishblue: "hsl(223, 64%, 98%)",
+      },
+      fontFamily: {
+        kumbh  : ['Kumbh Sans'],
+      }
+    },
   },
   plugins: [],
 }
