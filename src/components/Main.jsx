fd96e7c4558482832d9023c5a74788e5ef815a6c
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-06-06 00:10:36 +0000

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -95,12 +95,13 @@ function LightBoxOpen(){
           {imageCarousel}
         </ul>
         <Lightbox 
-        lbox={!lightbox}
-        closelbox={lbox => setLightbox(lbox => !lbox)}
-        image={data[currentIndex].url}
-        previousslide={prevSlide}
-        nextslide={nextSlide}
-        className='flex' />
+          lbox={!lightbox}
+          closelbox={lbox => setLightbox(lbox => !lbox)}
+          image={data[currentIndex].url}
+          previousslide={prevSlide}
+          nextslide={nextSlide}
+          className='flex' 
+        />
       </div>
       <div className='main--section p-6 md:p-[3.3rem] font-kumbh'>
         <h3 className='font-kumbh uppercase tracking-wide md:tracking-wider font-bold text-Orange'>sneaker company</h3>
@@ -120,7 +121,7 @@ function LightBoxOpen(){
           <p className='font-bold'>{count}</p>
           <button type='button' onClick={add}><img src={icon_plus} alt="" /></button>
         </div>
-        <div type='button' className='w-full md:basis-3/5 flex items-center justify-center mt-4 py-3 md:py-2 rounded-lg bg-Orange gap-4 hover:bg-opacity-75 transition-shadow ease-in-out'>
+        <div type='button'  className='w-full md:basis-3/5 flex items-center justify-center mt-4 py-3 md:py-2 rounded-lg bg-Orange gap-4 hover:bg-opacity-75 transition-shadow ease-in-out'>
         <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#ffffff" fillRule="nonzero"/></svg>
           <p className='text-white font-bold'>Add to Cart</p>
         </div>
